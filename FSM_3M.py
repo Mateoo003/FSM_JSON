9fa3cecb0e9867882fde11b9d0f350d61863690e
Mateoo0003 <mateoosp.003@gmail.com>
2022-10-17 21:09:59 -0500

    cambios finales

--- FSM_3M.py
+++ FSM_3M.py
@@ -1,7 +1,10 @@
+from datatest import input_test, output_test
 from read import archive
+import time
 
 parameters_FSM = archive["parameters"][0]
 inputs = archive["data_test"]["input"]
+outputs = archive["data_test"]["output"]
 
 def FSM_3M(state,input):
     if parameters_FSM["status_"+ str(state)]:
@@ -14,15 +17,47 @@ def FSM_3M(state,input):
 outputs = ""
 state = "0"
 
-for input in inputs[::-1]:
-    state, output = FSM_3M(int(state), int(input))
+# for input in inputs[::-1]:
+#     state, output = FSM_3M(int(state), int(input))
+#     outputs += str(output)
+ 
+count = 0
+
+for i in inputs[::-1]: 
+    print("Estado actual: "+state+". Entrada: "+i,end=". ")
+    state,output=FSM_3M(state,i)
+    output_test(output, count)
+    print("Estado siguiente: "+state+". Salida: "+output)
     outputs += str(output)
+    time.sleep(0.2)
+    if len(inputs)-1 == count:
+        if state == "0":
+            output = "00"
+            outputs += "00"
+            count += 1
+            for w in output:
+                print("Estado actual: "+state+". Salida: "+ w)
+                output_test(w, count)
+                count += 1
+                time.sleep(0.2)
+        elif state == "1":
+            output = "10"
+            outputs += "10"
+            count += 1
+            for w in output:
+                print("Estado actual: "+state+". Salida: "+ w)
+                output_test(w, count)
+                count += 1
+                time.sleep(0.2)
+        elif state == "2":
+            output = "01"
+            outputs += "01"
+            count += 1
+            for w in output:
+                print("Estado actual: "+state+". Salida: "+ w)
+                output_test(w, count)
+                count += 1
+                time.sleep(0.2)
+    count += 1
 
-if state == "0":
-    outputs += "00"
-elif state == "1":
-    outputs += "10"
-elif state == "2":
-    outputs += "01"
-
-print('input: ', inputs, ', outputs: ', outputs[::-1])
+# print('input: ', inputs, ', outputs: ', outputs[::-1])
--- datatest.py
+++ datatest.py
@@ -7,22 +7,18 @@ output_data_test = archive["data_test"]["output"]
 input_cont = 0
 output_cont = 0
 
-def input_test(bits):
+def input_test(bits,index):
     global input_cont
-    for i in bits[::-1]:
-        print(i)
-        assert i == str(i), f'The input requiered is {i} in {input_cont}'
-        input_cont+=1
-        if len(input_data_test) <= input_cont:
-            raise Exception(f'No more test data')
+    assert bits == list(input_data_test[::-1])[index], f'The input requiered is {bits} in {input_cont}'
+    input_cont+=1
+    if len(input_data_test) <= input_cont:
+        raise Exception(f'No more test data')
     return input_data_test
 
-def output_test(bits):
+def output_test(bits,index):
     global output_cont
-    for i in bits[::-1]:
-        print(i)
-        assert i == str(i), f'The input requiered is {i} in {output_cont}'
-        output_cont+=1
-        if len(output_data_test) <= output_cont:
-            raise Exception(f'No more test data')
+    assert bits == list(output_data_test[::-1])[index], f'The output requiered is {bits} in {output_cont}'
+    output_cont+=1
+    if len(output_data_test) <= output_cont:
+        raise Exception(f'No more test data')
     return output_data_test
